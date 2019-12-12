06e864b9f8bde9c9f7d7a3a2ee6b23db7884ac45
Duy P <dreamow9@gmail.com>
2019-12-13 04:33:05 +0700

    fix address & user

--- routes/address.js
+++ routes/address.js
@@ -5,6 +5,6 @@ var addressController = require('../controllers/addressController');
 var authController = require('../controllers/authController');
 
 // Route vào các controller
-router.get('/info', authController.isAuthenticated, addressController.get_user_data);
+router.get('/list', authController.isAuthenticated, addressController.get_user_address);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,6 @@ var usersController = require('../controllers/usersController');
 var authController = require('../controllers/authController');
 
 // Route vào các controller
-router.get('/info', authController.isAuthenticated, usersController.get_user_address);
+router.get('/info', authController.isAuthenticated, usersController.get_user_data);
 
 module.exports = router;
