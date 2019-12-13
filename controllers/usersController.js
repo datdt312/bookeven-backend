6954ad92184ff248765194b7c67b810594b24ab4
hope31299 <thanhdatba12@gmail.com>
2019-12-13 19:15:56 +0700

    fix get data user url

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,7 +2,7 @@ const database = require('../database/connection');
 
 exports.get_user_data = (req, res) => {
     try {
-        var body = req.query;
+        var body = req.params;
 
         database.query('SELECT id, email, fullname, phone, role FROM users WHERE id = ?', [body.id], (err, rows, fields) => {
             if (!err) {
@@ -11,14 +11,14 @@ exports.get_user_data = (req, res) => {
 
                     res.status(200).json(info);
                 } else {
-                    res.status(400).json({ message: "Something went wrong!" });
+                    res.status(400).json({ message: "Something went wrong!1" });
                 }
             } else {
                 console.log(err);
-                res.status(400).json({ message: "Something went wrong!" });
+                res.status(400).json({ message: "Something went wrong!2" });
             }
         });
     } catch (e) {
-        res.status(400).json({ message: "Something went wrong!", _error: e });
+        res.status(400).json({ message: "Something went wrong!3", _error: e });
     }
 };
