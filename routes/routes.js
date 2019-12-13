49e8ccb2485fadd1b5a6116d7144bca19e22d39a
username31299 <33161083+username31299@users.noreply.github.com>
2019-12-13 22:31:13 +0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const authController = require('../controllers/authController');
 const router = app => {
 
     app.get('/', (request, response) => {
-        response.send('<h1 style="transform: translate(50%, 200px);">chào mừng hải hải đến với thế giới api</h1>');
+        response.send('<h1 style="transform: translate(200px, 200px);">chào mừng hải hải đến với thế giới api</h1>');
     });
 
     app.use('/apis', require('./auth'));
