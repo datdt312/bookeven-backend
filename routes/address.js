1115274e2dfd5b3b645b481dce19d4d0fd98bed9
Duy P <dreamow9@gmail.com>
2019-12-15 00:24:28 +0700

    add order

--- routes/address.js
+++ routes/address.js
@@ -7,7 +7,7 @@ var authController = require('../controllers/authController');
 // Route vào các controller
 router.get('/list', authController.isAuthenticated, addressController.get_user_address);
 router.post('/new', authController.isAuthenticated, addressController.new);
-router.put('/update', authController.isAuthenticated,addressController.update);
-router.delete('/delete', authController.isAuthenticated,addressController.delete);
+router.put('/update', authController.isAuthenticated, addressController.update);
+router.delete('/delete', authController.isAuthenticated, addressController.delete);
 
 module.exports = router;
