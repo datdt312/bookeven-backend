ac0391d47e8df764c0c8d608197f8af04b138dbd
Duy P <dreamow9@gmail.com>
2019-12-15 21:13:38 +0700

    add detail and fix new

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -13,7 +13,20 @@ exports.filter = (req, res) => {
 
 //order detail
 exports.detail = (req, res) => {
-
+    let body = req.body;
+    try {
+        database.query(`SELECT * FROM orders WHERE id = ?;
+                        SELECT book_id, amount FROM orderdetails WHERE order_id = ?`,
+                        [body.id, body.id], (err, rows, fields) => {
+            if (!err) {
+                res.status(200).json({data: rows[0], books: rows[1]});
+            } else {
+                res.status(202).json({message: "Không thực hiện được yêu cầu" + err});
+            }
+        })
+    } catch (e) {
+        res.status(500).json({message: "Đã có lỗi xảy ra", _error: e});
+    }
 }
 
 //new nè
@@ -30,11 +43,10 @@ exports.add_order = (req, res) => {
                 //get order id after inserting
                 let order_id = rows[1][0].order_id;
                 // insert into orderdetail
-                database.query(`INSERT INTO orderdetails (book_id, order_id, amount) VALUES (
-                                (SELECT book_id FROM carts WHERE user_id = ?),
-                                ?,
-                                (SELECT amount FROM carts WHERE user_id = ?)
-                                );`, [user_id, order_id, user_id], (err, rows, fields) => {
+                database.query(`INSERT INTO orderdetails (book_id, order_id, amount) 
+                                SELECT book_id, (SELECT id FROM orders WHERE id = ?), amount 
+                                FROM carts WHERE user_id = ?
+                                ;`, [order_id, user_id], (err, rows, fields) => {
                     if (!err){
                         res.status(200).json({ order_id: order_id, message: "Tạo đơn hàng thành công" });
                         cartsController.remove_all_book(user_id);
