077f892aa61f594cf33a26d89787c67ddab4d8c4
hope31299 <thanhdatba12@gmail.com>
2019-12-21 09:53:13 +0700

    last christmas i give u my heart

--- helpers/booksHelper.js
+++ helpers/booksHelper.js
@@ -5,10 +5,11 @@ exports.book_format = (row) => {
     return {
         id: row.id,
         title: row.title,
-        author: row.author.replace(';', ", "),
+        author: row.author.split(';').join(', '),
         price: row.price,
         image: row.image,
         discount: row.discount,
-        inventory: row.inventory
+        inventory: row.inventory,
+        rate: row.rate
     };
 }
