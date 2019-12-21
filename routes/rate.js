5080282333df1d173654b4705f143022d719bf23
hope31299 <thanhdatba12@gmail.com>
2019-12-21 08:58:56 +0700

    change rate

--- routes/rate.js
+++ routes/rate.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-var rateController = require('../controllers/rateController');
+var ratesController = require('../controllers/ratesController');
 var authController = require('../controllers/authController');
 
 // Route vào các controller
-router.post('/new'/*, authController.isAuthenticated*/, rateController.addRate);
-router.post('/list'/*, authController.isAuthenticated*/, rateController.list);
-router.put('/update'/*, authController.isAuthenticated*/, rateController.update);
+router.post('/new', authController.isAuthenticated, rateController.addRate);
+router.post('/list', rateController.list);
+router.put('/update', authController.isAuthenticated, rateController.update);
 
 module.exports = router;
